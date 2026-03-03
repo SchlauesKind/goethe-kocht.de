e67b4031e2f9f597ff7de40bbfb2180b6a879c77
SchlauesKind <roma.volt1125@gmail.com>
2026-03-03 21:04:00 +0100

    icons-added

--- src/conponents/header_component.tsx
+++ src/conponents/header_component.tsx
@@ -1,12 +1,27 @@
+import goethe_logo from "../assets/goethe_logo.jpg";
+import cook_icon from "../assets/start_to_cook.png";
+import home_icon from "../assets/home_icon.svg";
 import styles from "./header_styles.module.css";
-import { Link } from "react-router-dom"
-
+import { Link } from "react-router-dom";
 
 function HeaderComponent() {
   return (
     <header className={`${styles.header} ${styles.flexContainer}`}>
-      <Link to="/" className={styles.item}>Zu Homepage</Link>
-      <Link to="/" className={styles.item}>Zu Homepage</Link>
+      <Link to="/" className={styles.item}>
+        <img src={home_icon} alt="Home Icon" className={styles.logo} />
+      </Link>
+      <img
+        src={cook_icon}
+        alt="Cook Icon"
+        className={`${styles.logo} ${styles.item}`}
+      />
+      <a
+        href="https://www.goethegymnasiumdortmund.de"
+        className={styles.item}
+        target="_blank"
+      >
+        <img src={goethe_logo} alt="Goethe Logo" className={styles.logo} />
+      </a>
     </header>
   );
 }
