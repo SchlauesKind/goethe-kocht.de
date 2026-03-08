769091be3509ac4ea9f9df98586e9c42cefd3d69
SchlauesKind <roma.volt1125@gmail.com>
2026-03-08 10:59:19 +0100

    local-node-repair-and-first-draft-for-homepage

--- src/pages/home_page.tsx
+++ src/pages/home_page.tsx
@@ -1,16 +1,21 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
+import styles from "./main_styles.module.css";
 
 function HomePage() {
   return (
-    <div className="homePageBody">
-      <h1>Home Page</h1>
-      <h2>Hier sind links zu Ländern deren Küchen wir haben:</h2>
+    <div className={styles.homePageBody}>
+      <h1 className={styles.homePageHeader}>Willkommen auf unserer Seite</h1>
+      <h2>Entdecken Sie unsere verschiedenen Küchen</h2>
       <ul>
         <li>
-          <Link to="/russian_kitchen">Russische Küche</Link>
+          <Link className={styles.link} to="/russian_kitchen">
+            Russische Küche
+          </Link>
         </li>
         <li>
-          <Link to="/russian_kitchen">Russische Küche</Link>
+          <Link className={styles.link} to="/german_kitchen">
+            Deutsche Küche
+          </Link>
         </li>
       </ul>
     </div>
