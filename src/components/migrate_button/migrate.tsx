e5abc74b524109bb30c720fff68a9e01e2b6bf25
Roman Fedorov <roma.volt1125@gmail.com>
2026-03-19 13:13:57 +0000

    plate-temp2

--- src/components/migrate_button/migrate.tsx
+++ src/components/migrate_button/migrate.tsx
@@ -3,15 +3,17 @@ import styles from "./migrate.module.css"
 function MigratePlate () {
 
     return (
-        <div className={styles.plateContainer}>
-            <h1>Diese seite ist nicht der neusten Version!</h1>
-            <p>
-                Sie verwenden gerade die Webseite, die ihrer Entwicklung und Aktualisierung eingefroren. <br />
-                Wechseln Sie zu der aktuellsten Seite um Kochbuch 2.0 zu erleben!
-            </p>
-            <div className={styles.buttonContainer}>
-                <a className={`${styles.migrateButton} ${styles.button}`} href="https://schlaueskind.github.io/goethe-kocht.de/">Akzeptieren</a>
-                <div className={`${styles.stayButton} ${styles.button}`}>Ablehnen</div>
+        <div className={styles.backPlate}>
+            <div className={styles.plateContainer}>
+                <div><h1>Diese Seite kann outdated sein</h1>
+                <p>
+                    Sie verwenden gerade die Webseite, die in ihrer Entwicklung und Aktualisierung eingefroren ist. <br />
+                    Wechseln Sie zu der aktuellsten Seite um Kochbuch 2.0 zu erleben!
+                </p></div>
+                <div className={styles.buttonContainer}>
+                    <a className={`${styles.migrateButton} ${styles.button}`} href="https://schlaueskind.github.io/goethe-kocht.de/">Akzeptieren</a>
+                    <div className={`${styles.stayButton} ${styles.button}`}>Ablehnen</div>
+                </div>
             </div>
         </div>
     );
